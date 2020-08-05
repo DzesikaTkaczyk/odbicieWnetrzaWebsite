92c83df662c89d9638d2984d4f52fd5c045801a9
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2020-08-05 22:44:29 +0200

    add mobile navrbar

---
+++ js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+window.addEventListener ? 
+window.addEventListener("load",script_js,false) : 
+window.attachEvent && window.attachEvent("onload",script_js);
+
+function script_js () {
+	const burger = document.getElementById('hamburger');
+	function toggleMenu(visible) { 
+	  document.querySelector('.mobile_menu').classList.toggle('show_mobile_menu', visible);
+	}
+
+	burger.addEventListener('click', function(e) {
+	  e.preventDefault();
+	  toggleMenu();
+    });
+
+    const smile = document.getElementById('smile')
+    function toggleColors() { 
+        document.querySelector('.day_mode').classList.toggle('.night_mode');
+    }
+
+    smile.addEventListener('click', function(e){
+        e.preventDefault();
+        toggleColors()
+    })
+
+}
