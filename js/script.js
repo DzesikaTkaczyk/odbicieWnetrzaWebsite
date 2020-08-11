39878b8323eb28212422ad860bb54d3f8819cc97
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2020-08-11 11:59:51 +0200

    add carousels

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//navbar
 window.addEventListener ? 
 window.addEventListener("load",script_js,false) : 
 window.attachEvent && window.attachEvent("onload",script_js);
@@ -24,5 +25,58 @@ function script_js () {
         e.preventDefault();
         toggleColors()
     })
+  }
 
+//carousels
+const carousels = document.querySelectorAll('.carousel');
+const carouselsLen = carousels.length
+
+const previous = document.querySelectorAll('.prev');
+const next = document.querySelectorAll('.next');
+let images = [];
+let imagesLen = [];
+
+for (let i = 0; i < carouselsLen; i++) {
+  images[i] = carousels[i].querySelectorAll('.carousel-photo');
+  imagesLen[i] = images[i].length;
+  previous[i].addEventListener('click', prevPicture);
+  next[i].addEventListener('click', nextPicture);
 }
+
+function isActive (carouselNr) {
+  for (let i = 0; i < imagesLen[carouselNr]; i++) {
+    if (images[carouselNr][i].classList.contains('carousel-active')) {
+      return i;
+    }
+  }
+}
+
+function nextPicture () {
+  for( let j = 0; j < carouselsLen; j++ ){
+    if (carousels[j] == this.parentElement){
+      let i = isActive(j);
+      images[j][i].classList.remove('carousel-active');
+      i += 1;
+      if (i >= imagesLen[j]) {
+        i = 0;
+      }
+      images[j][i].classList.add('carousel-active');
+    }
+  }
+}
+
+function prevPicture () {
+  for( let j = 0; j < carouselsLen; j++){
+    if(carousels[j] == this.parentElement){
+      let i = isActive(j);
+      images[j][i].classList.remove('carousel-active');
+      i -= 1;
+      if (i < 0) {
+        i = imagesLen[j] - 1;
+      }
+      images[j][i].classList.add('carousel-active');
+    }
+  }
+}
+
+//day/night
